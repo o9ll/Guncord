10dc42c7846cc88f66dc450869bd8c69506b97fc
o9ll <229858305+o9ll@users.noreply.github.com>
2026-08-05 21:22:35 +0300

    build: release v1.0.8 - Guncord v1.0.8

--- installer/src/renderer/routes.js
+++ installer/src/renderer/routes.js
@@ -1,10 +1,11 @@
 import LoadingPage from "./pages/Loading.svelte";
+import LicensePage from "./pages/License.svelte";
 import ActionsPage from "./pages/Actions.svelte";
 import PlatformsPage from "./pages/Platforms.svelte";
 import PerformActionPage from "./pages/PerformAction.svelte";
 
 export default {
-    "/": PlatformsPage,
+    "/": LicensePage,
     "/actions": ActionsPage,
     "/setup/:action": PlatformsPage,
     "/install": PerformActionPage,
